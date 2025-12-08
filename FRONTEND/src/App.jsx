c39018c062c59e5feacbd94118ec81b839b71de7
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2025-12-08 16:55:02 +0100

    FEAT : FT-11 : initialize frontend with Vite, React, and Tailwind CSS and required dependencies

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,12 @@
+import { FaChild } from "react-icons/fa";
+
+function App() {
+
+  return (
+    <>
+      <h1 className="flex items-center text-red-500">Hello Fleet Track <FaChild /></h1>
+    </>
+  )
+}
+
+export default App
